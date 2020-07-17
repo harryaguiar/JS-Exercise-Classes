cd0ab8f2c96dc338d116a2e21ee7d83765831c92
Harrison Aguiar <harryaguiar@gmail.com>
2020-07-16 22:03:39 -0400

    working in task 2

--- index.js
+++ index.js
@@ -47,7 +47,15 @@ class Person {
     this.stomach = [];
   }
   eat(food){
-    this.stomach = stomach.push(food)
+    if(this.stomach.length < 10){
+      this.stomach.push(food);
+    }
+  }
+  poop(){
+    this.stomach = []
+  }
+  toString(){
+    return `${this.name}, ${this.age}`
   }
 
 }
@@ -67,6 +75,20 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank += gallons;
+  }
+  drive(distance){
+    this.odometer += distance;
+    this.tank -= distance * this.milesPerGallon;
+  }
+
 
 }
 
